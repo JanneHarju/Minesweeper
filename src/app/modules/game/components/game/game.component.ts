2fbd29780e49c207402f0038e0427bbb226e32c9
Janne Harju <janne.harju@leanware.fi>
2021-09-27 16:37:09 +0300

    files are style formatted. adding timer to UI. trying to get angular more optimized with big minefields. added few form validations.

--- src/app/modules/game/components/game/game.component.ts
+++ src/app/modules/game/components/game/game.component.ts
@@ -1,11 +1,19 @@
 import { GameSettings } from './../../gameSettings';
 import { InfoModalComponent } from './../info-modal/info-modal.component';
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Fireworks } from 'fireworks-js'
+import { Fireworks } from 'fireworks-js';
 import { fireWorksConfig } from './fireWorksConfig';
 import { StartModalComponent } from '../start-modal/start-modal.component';
-import { timer } from 'rxjs';
+import { Observable, Subject, timer } from 'rxjs';
+import { map, repeatWhen, switchMap, takeUntil } from 'rxjs/operators';
 
 export enum CellStatus {
   Origin,
@@ -13,7 +21,7 @@ export enum CellStatus {
   Denoted,
   Number,
   Empty,
-  Unknown
+  Unknown,
 }
 
 export interface Cell {
@@ -22,31 +30,55 @@ export interface Cell {
   status: CellStatus;
   isMine: boolean;
   mineCountAround?: number;
+  id: number;
 }
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-
   flaggedMineCount = 0;
   denotedCount = 0;
   flaggedCount = 0;
   startTime!: Date;
   settings: GameSettings = new GameSettings();
   lives = 3;
-  cells: Cell[] = []
+  cells: Cell[] = [];
   CellStatus = CellStatus;
+  secondTimer!: Observable<void>;
+
+  private readonly _stop = new Subject<void>();
+  private readonly _start = new Subject<void>();
+
+  currentTime!: string;
 
   @ViewChild('fireworks')
   fireworksDiv!: ElementRef;
 
   gameFinished = false;
 
-  constructor(private cdr: ChangeDetectorRef, private dialogService: MatDialog) { }
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private dialogService: MatDialog
+  ) {
+    this.secondTimer = timer(0, 1000).pipe(
+      map((value) => {
+        this.updateTimer();
+      }),
+      takeUntil(this._stop),
+      repeatWhen(() => this._start)
+    );
+  }
 
+  private updateTimer() {
+    const stopTime = new Date();
+    const diffTimeInMilliseconds = Math.abs(
+      stopTime.valueOf() - this.startTime.valueOf()
+    );
+    this.currentTime = this.millisToMinutesAndSeconds(diffTimeInMilliseconds);
+  }
 
   ngOnInit(): void {
     this.showStartModal();
@@ -60,75 +92,129 @@ export class GameComponent implements OnInit {
     while (mineLocations.length < this.settings.mineTotalCount) {
       const mineLocationY = Math.floor(this.settings.rows * Math.random());
       const mineLocationX = Math.floor(this.settings.columns * Math.random());
-      if (!mineLocations.find(x => x.X === mineLocationX && x.Y === mineLocationY)) {
+      if (
+        !mineLocations.find(
+          (x) => x.X === mineLocationX && x.Y === mineLocationY
+        )
+      ) {
         mineLocations.push({ X: mineLocationX, Y: mineLocationY } as Cell);
       }
     }
+    let id = 0;
     for (let xIndex = 0; xIndex < this.settings.columns; xIndex++) {
       for (let yIndex = 0; yIndex < this.settings.rows; yIndex++) {
-        const isMine = mineLocations.find(x => x.X === xIndex && x.Y === yIndex) !== undefined;
-        this.cells.push(
-          {
-            X: xIndex,
-            Y: yIndex,
-            status: CellStatus.Origin,
-            isMine: isMine,
-            mineCountAround: 0
-          } as Cell);
+        const isMine =
+          mineLocations.find((x) => x.X === xIndex && x.Y === yIndex) !==
+          undefined;
+        this.cells.push({
+          id: id,
+          X: xIndex,
+          Y: yIndex,
+          status: CellStatus.Origin,
+          isMine: isMine,
+          mineCountAround: 0,
+        } as Cell);
+        id++;
       }
     }
-    this.cells.forEach(cell => {
+    this.cells.forEach((cell) => {
       if (!cell.isMine) {
-        let mineCount = this.cells.filter(x => (cell.X - 2) < x.X && (cell.X + 2) > x.X &&
-          (cell.Y - 2) < x.Y && (cell.Y + 2) > x.Y && x.isMine).length;
+        let mineCount = this.cells.filter(
+          (x) =>
+            cell.X - 2 < x.X &&
+            cell.X + 2 > x.X &&
+            cell.Y - 2 < x.Y &&
+            cell.Y + 2 > x.Y &&
+            x.isMine
+        ).length;
         cell.mineCountAround = mineCount !== 0 ? mineCount : undefined;
       }
     });
+    console.log('Valmis');
+  }
+
+  cellSize(): string {
+    const maxLength = Math.max(this.settings.columns, this.settings.rows);
+    if (maxLength < 13) {
+      return '50px';
+    } else {
+      return '30px';
+    }
+  }
+
+  identify(index: number, item: Cell) {
+    return item.id;
   }
 
   cellClicked(event: MouseEvent, item: Cell) {
     event.preventDefault();
     event.stopPropagation();
     this.handleMouseLeftClick(item);
-    this.denotedCount = this.cells.filter(x => x.status === CellStatus.Denoted).length;
+    this.denotedCount = this.cells.filter(
+      (x) => x.status === CellStatus.Denoted
+    ).length;
     this.lives = this.settings.maxLives - this.denotedCount;
     if (this.lives < 0) {
-      this.dialogService.open(InfoModalComponent, { data: { header: 'Hävisit. Yritä uudestaan.' } }).afterClosed().subscribe(x => {
-        this.showStartModal();
-      });
+      this.dialogService
+        .open(InfoModalComponent, {
+          data: { header: 'Hävisit. Yritä uudestaan.' },
+        })
+        .afterClosed()
+        .subscribe((x) => {
+          this.showStartModal();
+        });
     } else {
       this.checkIsGameFinished();
     }
   }
 
   private checkIsGameFinished() {
-    const notOriginCount = this.cells.filter(x => x.status === CellStatus.Origin).length;
+    const notOriginCount = this.cells.filter(
+      (x) => x.status === CellStatus.Origin
+    ).length;
     if (notOriginCount == 0 && this.lives >= 0) {
       this.gameFinished = true;
       const stopTime = new Date();
-      const diffTimeInMilliseconds = Math.abs(stopTime.valueOf() - this.startTime.valueOf());
+      const diffTimeInMilliseconds = Math.abs(
+        stopTime.valueOf() - this.startTime.valueOf()
+      );
       const resultTime = this.millisToMinutesAndSeconds(diffTimeInMilliseconds);
+      this._stop.next();
       this.cdr.detectChanges();
-      const fireworks = new Fireworks(this.fireworksDiv.nativeElement, fireWorksConfig);
-      this.dialogService.open(InfoModalComponent, { data: { header: 'Onnittelut pelin läpäisystä', resultTime: resultTime } }).afterClosed().subscribe(x => {
-        this.showStartModal();
-        fireworks.stop();
-      });
+      const fireworks = new Fireworks(
+        this.fireworksDiv.nativeElement,
+        fireWorksConfig
+      );
+      this.dialogService
+        .open(InfoModalComponent, {
+          data: {
+            header: 'Onnittelut pelin läpäisystä',
+            resultTime: resultTime,
+          },
+        })
+        .afterClosed()
+        .subscribe((x) => {
+          this.showStartModal();
+          fireworks.stop();
+        });
       fireworks.start();
     }
   }
   private millisToMinutesAndSeconds(millis: number): string {
     let minutes = Math.floor(millis / 60000);
     let seconds = +((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
   }
   private showStartModal() {
-    this.dialogService.open(StartModalComponent).afterClosed().subscribe((x: GameSettings) => {
-      this.startTime = new Date();
-      const secondTimer =  timer(1000, 1000);
-      this.settings = x;
-      this.initialGame();
-    });
+    this.dialogService
+      .open(StartModalComponent)
+      .afterClosed()
+      .subscribe((x: GameSettings) => {
+        this.startTime = new Date();
+        this.settings = x;
+        this.initialGame();
+        this._start.next();
+      });
   }
 
   handleMouseLeftClick(item: Cell) {
@@ -137,10 +223,16 @@ export class GameComponent implements OnInit {
     } else {
       item.status = CellStatus.Number;
       if (item.mineCountAround == undefined) {
-        const cellsAround = this.cells.filter(x =>
-          (item.X - 2) < x.X && (item.X + 2) > x.X &&
-          (item.Y - 2) < x.Y && (item.Y + 2) > x.Y && !(x.X == item.X && x.Y == item.Y) && x.status == CellStatus.Origin);
-        cellsAround.forEach(x => this.handleMouseLeftClick(x));
+        const cellsAround = this.cells.filter(
+          (x) =>
+            item.X - 2 < x.X &&
+            item.X + 2 > x.X &&
+            item.Y - 2 < x.Y &&
+            item.Y + 2 > x.Y &&
+            !(x.X == item.X && x.Y == item.Y) &&
+            x.status == CellStatus.Origin
+        );
+        cellsAround.forEach((x) => this.handleMouseLeftClick(x));
       }
     }
   }
@@ -155,7 +247,9 @@ export class GameComponent implements OnInit {
     } else if (item.status === CellStatus.Unknown) {
       item.status = CellStatus.Origin;
     }
-    this.flaggedCount = this.cells.filter(x => x.status === CellStatus.Flagged).length;
+    this.flaggedCount = this.cells.filter(
+      (x) => x.status === CellStatus.Flagged
+    ).length;
 
     this.checkIsGameFinished();
     return false;
--- src/app/modules/game/components/start-modal/start-modal.component.ts
+++ src/app/modules/game/components/start-modal/start-modal.component.ts
@@ -10,14 +10,14 @@ export class StartModalComponent implements OnInit {
 
   settings: GameSettings = new GameSettings();
   constructor() {
-    
+
   }
 
   ngOnInit(): void {
     this.settings = new GameSettings();
-    this.settings.rows = 10;
-    this.settings.columns = 10;
-    this.settings.mineTotalCount = 10;
+    this.settings.rows = 20;
+    this.settings.columns = 20;
+    this.settings.mineTotalCount = 50;
     this.settings.maxLives = 3;
   }
 
